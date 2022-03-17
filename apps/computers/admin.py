3ca015af447857799b79fa18c5f6607222a44066
Yaroslav <maverick032695@gmail.com>
2022-03-17 12:49:29 +0200

    Homework from lesson 2

--- apps/computers/admin.py
+++ apps/computers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import ComputerModel
 # Register your models here.
+@admin.site.register(ComputerModel)
+class ComputerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand', 'model', 'ram', 'screen')
