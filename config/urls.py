822e866d7fcd72a71ade5a620634a3c6e3fa91d4
Yaroslav <maverick032695@gmail.com>
2022-03-17 13:05:49 +0200

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import UsersListCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', UsersListCreateView.as_view())
 ]
