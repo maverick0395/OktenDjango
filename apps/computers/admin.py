43d61136c564ad20daeb771aa9d3037cdca58327
Yaroslav <maverick032695@gmail.com>
2022-03-17 12:58:31 +0200

    admin.py fix

--- apps/computers/admin.py
+++ apps/computers/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from .models import ComputerModel
-# Register your models here.
-@admin.site.register(ComputerModel)
+
+
+@admin.register(ComputerModel)
 class ComputerAdmin(admin.ModelAdmin):
     list_display = ('id', 'brand', 'model', 'ram', 'screen')
